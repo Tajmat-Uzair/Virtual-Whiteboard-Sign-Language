1d00cdc4bb342735ea76909e09fc03e660496b9c
Shivanii30 <kshirsagarshivani1438@gmail.com>
2023-11-08 13:53:12 +0530

    Recognizes A,B,C

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import time
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
-classifier = Classifier("Model/keras_model.h5","Model/label.text")
+classifier = Classifier("Model/keras_model.h5","Model/labels.txt")
 
 offset = 20 #to give extra space while cropping
 imgSize = 300 #to solve the dimension pblm while cropping
@@ -20,6 +20,7 @@ labels = ["A","B","C"]
 while True:
     success,img = cap.read()
     img = cv2.flip(img, 1)
+    imgOutput = img.copy()
     hands,img = detector.findHands(img)
 
     if hands: 
@@ -42,7 +43,7 @@ while True:
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize-wCal)/2)
             imgWhite[:,wGap:wCal+wGap]= imgResize #overlay img on white
-            prediction, index = classifier.getPrediction(img)
+            prediction, index = classifier.getPrediction(imgWhite)
             print(prediction,index)
         
         else :
@@ -52,7 +53,11 @@ while True:
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize-hCal)/2)
             imgWhite[hGap:hCal+hGap,:]= imgResize #overlay img on white
+            prediction, index = classifier.getPrediction(imgWhite,draw=False)
 
+        cv2.rectangle(imgOutput,(x-offset,y-offset-50),(x-offset+90,y-offset-50+50),(255,0,255),cv2.FILLED)
+        cv2.putText(imgOutput,labels[index],(x,y-26),cv2.FONT_HERSHEY_COMPLEX,1.7,(255,255,255),2)
+        cv2.rectangle(imgOutput,(x-offset,y-offset),(x+w+offset,y+h+offset),(255,0,255),4)
 
 
 
@@ -62,7 +67,7 @@ while True:
         cv2.imshow("Image White", imgWhite)
 
 
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", imgOutput)
 
     cv2.waitKey(1) 
  
